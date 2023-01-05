9ac3951bca3fc88aef1649459302428ab9552101
Muhamed Medhat <60796459+iNightjar@users.noreply.github.com>
2023-01-05 22:11:54 +0200

    Time: 206 ms (63.47%), Space: 15.6 MB (11.18%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -1,18 +1,18 @@
-class Solution(object):
-    def pivotIndex(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int: # function annotation
         if len(nums) == 0:
             return -1
-        left = [0, nums[0]]
-        right = [0, nums[-1]]
+        leftSum = [0, nums[0]]
+        rightSum = [0, nums[-1]]
         for i in range(1, len(nums) - 1):
-            left.append(nums[i] + left[-1])
-            right.append(nums[len(nums) - 1 - i] + right[-1])
-        right = right[::-1]
-        for i in range(len(left)):
-            if left[i] == right[i]:
+            # print(nums[i])
+            # print(leftSum[-1])
+            leftSum.append(nums[i] + leftSum[-1])
+            rightSum.append(nums[len(nums)-1-i] + rightSum[-1])
+        rightSum = rightSum[::-1]
+        print(leftSum)
+        print(rightSum)
+        for i in range(len(leftSum)):
+            if leftSum[i] == rightSum[i]:
                 return i
         return -1
