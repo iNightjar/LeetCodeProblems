b4e05a33a243526cc76e646dc18bec6dc290849b
Muhamed Medhat <60796459+iNightjar@users.noreply.github.com>
2023-01-25 22:59:53 +0200

    Time: 690 ms (29.73%), Space: 28.8 MB (12.16%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,3 +1,8 @@
-class Solution:
-    def containsDuplicate(self, nums: list[int]) -> bool:
-        return len(set(nums)) != len(nums)
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hset = set()
+        for idx in nums:
+            if idx in hset:
+                return True
+            else:
+                hset.add(idx)
