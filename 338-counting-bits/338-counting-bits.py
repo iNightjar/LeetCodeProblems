bdd5229d9c6b824eb8cbf001a35172ffacf347f3
Muhamed Medhat <60796459+iNightjar@users.noreply.github.com>
2023-04-30 22:09:16 +0200

    Time: 89 ms (56.79%), Space: 23.1 MB (5.45%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,9 +1,9 @@
 class Solution:
     def countBits(self, n) -> list[int]:
-        dp = [0] * (n + 1)
+        dp_dynamic = [0] * (n + 1)
         offset = 1
-        for i in range(1, n + 1):
-            if offset * 2 == i:
-                offset = i
-            dp[i] = 1  + dp[i - offset]
-        return dp
+        for iterator in range(1, n + 1):
+            if offset * 2 == iterator:
+                offset = iterator
+            dp_dynamic[iterator] = 1  + dp_dynamic[iterator - offset]
+        return dp_dynamic
